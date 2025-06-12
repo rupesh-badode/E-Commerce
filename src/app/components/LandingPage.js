f94ca05462ff60c48accc4daa22597ff1d099bf5
Rupesh Badode <rupeshbadode77@gmail.com>
2025-06-12 11:07:36 +0530

    Update LandingPage.js

--- src/app/components/LandingPage.js
+++ src/app/components/LandingPage.js
@@ -4,7 +4,6 @@ import { useEffect } from "react";
 import AOS from "aos";
 import 'aos/dist/aos.css';
 import Image from "next/image";
-import { auth } from "../utils/firebaseConfig";
 
 export default function LandingPage() {
   useEffect(() => {
