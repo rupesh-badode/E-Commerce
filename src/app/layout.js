0187bada78f8d8577de97dfbc4fc0587f65ac9e0
Rupesh Badode <rupeshbadode77@gmail.com>
2025-06-12 10:59:05 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({children}) {
           <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossOrigin="anonymous"/>
           <Script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js" integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossOrigin="anonymous"></Script>
           <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"></link>
-          <link rel="icon" href="../public/favicon.ico"/>
+          <link rel="icon" href="/public/favicon.ico"/>
         </head>
         <CartProvider>
           <body>
